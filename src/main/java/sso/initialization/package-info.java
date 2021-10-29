2b31fd12eb33b48fce582361862df12229d1b67f
Adam Klinkosz <adam.klinkosz@onegini.com>
2021-10-29 09:23:02 +0200

    Initial commit

---
+++ src/main/java/sso/initialization/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Main application init classes.
+ *
+ * Examples:
+ *    ApplicationInitializer - main application init
+ * 		DatasourceInitialization - all datasource initialization logic
+ * 		TimeInitialization - Clock and stuff
+ * 		# ... etc
+ *
+ */
+package sso.initialization;
---
+++ src/main/java/sso/util/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * A place for utility classes, common to more than one module. Basically these are a possible candidates for a separate library.
+ *
+ * Examples:
+ * - domain/
+ * -- BaseValueObject
+ * -- Failure
+ * -- Limit
+ * -- Result
+ * - sql2o/converter/
+ * -- BaseValueObjectReadConverter
+ * -- LocalDateTimeConverter
+ * - string/
+ * -- CharacterRepetitionUtil
+ * -- StringGenerator
+ * - transaction/
+ * -- TransactionManager
+ * -- UseCaseTransactionalProxy
+ * - transformation
+ * -- JsonTransformer
+ * -- CsvTransformer
+ * -- XmlTranfsormer
+ * - validation/
+ * -- Preconditions
+ * -- Validated
+ * -- ValidationError
+ * -- ValidationException
+ * - ... etc
+ */
+package sso.util;
