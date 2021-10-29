026ee62868e9a9cd0d6eebabd45e480ba6a3041c
Adam Klinkosz <adam.klinkosz@onegini.com>
2021-10-29 14:35:42 +0200

    Rename Facebook authenticating service implementation

--- src/main/java/sso/domain/facebook/provider/FacebookAuthenticationStubService.java
+++ src/main/java/sso/domain/facebook/provider/FacebookAuthenticationStubService.java
@@ -11,7 +11,7 @@ import sso.domain.facebook.core.gateway.FacebookAuthenticationService;
 import sso.domain.facebook.provider.model.FacebookDatabaseEntry;
 import sso.util.domain.Result;
 
-public class FacebookAuthenticationWebService implements FacebookAuthenticationService {
+public class FacebookAuthenticationStubService implements FacebookAuthenticationService {
   private static final Map<String, FacebookDatabaseEntry> USER_DATABASE = Map.of(
       "d80e08d6", FacebookDatabaseEntry.of("43cc8f2e-389d-11ec-b097-ef579a0a53dd", "John doe"),
       "14630188", FacebookDatabaseEntry.of("4888bb14-389d-11ec-bea0-c7563e55e7bc", "Alfredo Arachidi")
