4c48a9c2849abdede551fcf3f086429ddcf5e646
Adam Klinkosz <spyro@o2.pl>
2021-11-05 11:54:41 +0100

    Encapsulate Facebook related classes to the facebook module

--- src/main/java/sso/domain/authentication/config/AuthenticationDomainConfiguration.java
+++ src/main/java/sso/domain/authentication/config/AuthenticationDomainConfiguration.java
@@ -4,33 +4,11 @@ import javax.inject.Singleton;
 
 import org.codejargon.feather.Provides;
 
-import sso.domain.authentication.core.usecase.AuthenticateWithFacebookUseCase;
 import sso.domain.authentication.core.usecase.AuthenticateWithUsernameAndPasswordUseCase;
-import sso.domain.authentication.entrypoint.AuthenticateWithFacebookController;
 import sso.domain.authentication.entrypoint.AuthenticateWithUsernameAndPasswordController;
-import sso.domain.facebook.core.gateway.FacebookAuthenticationService;
-import sso.domain.facebook.provider.FacebookAuthenticationStubService;
 import sso.domain.user.core.gateway.UserRepository;
 
 public class AuthenticationDomainConfiguration {
-
-  @Provides
-  @Singleton
-  public FacebookAuthenticationService facebookAuthenticationService() {
-    return new FacebookAuthenticationStubService();
-  }
-
-  @Provides
-  @Singleton
-  public AuthenticateWithFacebookUseCase authenticateWithFacebookUseCase(FacebookAuthenticationService facebookAuthenticationService) {
-    return new AuthenticateWithFacebookUseCase(facebookAuthenticationService);
-  }
-
-  @Provides
-  @Singleton
-  public AuthenticateWithFacebookController authenticateWithFacebookController(AuthenticateWithFacebookUseCase authenticateWithFacebookUseCase) {
-    return new AuthenticateWithFacebookController(authenticateWithFacebookUseCase);
-  }
 
   @Provides
   @Singleton
